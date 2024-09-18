6fdd2870058e5e4928d4fa111aee8a23e8d6baf9
hajar <elamiryhajar801@gmail.com>
2024-09-18 23:35:53 +0100

    .

--- project/mon_application/models.py
+++ project/mon_application/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class profile(models.Model):
+    user=
+    id_user=
+    bio=
+    profileimg=models.ImageField(upload_to='profile_image')
+    location=
