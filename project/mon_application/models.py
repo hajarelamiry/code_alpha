47a1a4327ac5e850e922a0b09d1af60e069b32af
hajar <elamiryhajar801@gmail.com>
2024-09-20 00:37:50 +0100

    last modifications

--- project/mon_application/models.py
+++ project/mon_application/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+user=get_user_model()
 
 # Create your models here.
 class profile(models.Model):
-    user=
-    id_user=
-    bio=
-    profileimg=models.ImageField(upload_to='profile_image')
-    location=
+    user=models.ForeignKey(user, on_delete=models.CASCADE)
+    id_user=models.IntegerField()
+    bio=models.TextField(blank=True)
+    profileimg=models.ImageField(upload_to='profile_image',default='avatar-3814049_1280.png')
+    location=models.CharField(max_length=100,blank=True)
+    
